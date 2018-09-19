2df523ef2de4a0177070ec09ce20fe15c48ca903
Jayhua <jordanhua97@gmail.com>
2018-09-19 13:29:08 -0700

    removed sidebar and added additional styling

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -5,8 +5,3 @@ setTimeout(function() {
     $(".description").fadeTo("fast", 1);
     $("#buttonsu").fadeTo("fast", 1);
 }, 1250);
-
-$("#su-btn").on("click", function(event) {
-    event.preventDefault();
-    window.location.href="/dashboard";
-})

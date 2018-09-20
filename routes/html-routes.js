60741592c0e47e7911a109c6e4b82f653c3796ab
gkourey <grant.kourey@gmail.com>
2018-09-19 17:31:59 -0700

    adding changes again

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -71,40 +71,6 @@ module.exports = function(app) {
     }).then(function(transactions){
        var tickerTally = {}
 
-<<<<<<< HEAD
-      transactions.forEach(tran => {
-      var obj = tran.dataValues; // all the keys in a transaction
-      var sym = obj.ticker;
-      if(tickerTally[sym]==undefined){
-        tickerTally[sym] = 
-        {
-          'ticker': sym,
-          'quantity': parseInt(obj.quantity),
-          'total': parseFloat(obj.total_price),
-        }
-      }
-      else{
-        tickerTally[sym].quantity += parseInt(obj.quantity);
-        tickerTally[sym].total += parseFloat(obj.total_price);
-      }
-    })
-    var stocks = [];
-    for(key in tickerTally){
-      stocks.push(tickerTally[key]);
-    }
-    db.User.findOne({
-      where: {id: req.user.id}
-    }).then(function(user){
-      res.render("dashboard", {
-        msg: "Welcome back",
-        name: user.firstName,
-        total: user.initialCash,
-        available: user.activeCash,
-        stock: stocks
-      }); 
-    })
-  })
-=======
           transactions.forEach(tran => {
             var obj = tran.dataValues; // all the keys in a transaction
             var sym = obj.ticker;
@@ -139,7 +105,6 @@ module.exports = function(app) {
               }); 
             })
           })
->>>>>>> 5a4ae94038f8ea5c8c4a27fed4c4f62e6c18c3dc
          
   //404 error page
   app.get("/*",function(req,res){
